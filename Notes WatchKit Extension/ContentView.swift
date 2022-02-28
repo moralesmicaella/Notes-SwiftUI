06d10e3bbbe2f3308652d67eb48815326c87a850
Micaella Morales <morales.micaella@gmail.com>
2022-02-28 03:26:28 -0800

    Initial Commit

---
+++ Notes WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notes WatchKit Extension
+//
+//  Created by Micaella Morales on 2/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Micaella Morales on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
